2419778a1d4399d4a0b794bfb3d240f237a8af59
root <barbarahayd@hotmail.com>
2025-03-02 19:43:41 +0000

    altera metodo http e habilita exibicao de erros na response do endpoint

--- meudominio/exemploinfra-rest/api/lertudo.php
+++ meudominio/exemploinfra-rest/api/lertudo.php
@@ -1,13 +1,13 @@
 <?php
 header("Access-Control-Allow-Origin: *");
 header("Content-Type: application/json; charset=UTF-8");
-header("Access-Control-Allow-Methods: GET"); // Alterado para GET
+header("Access-Control-Allow-Methods: GET");
 header("Access-Control-Max-Age: 3600");
 header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
 
-ini_set('display_errors', 1); // Habilitar exibição de erros
-error_reporting(E_ALL); // Mostrar todos os erros
-
+ini_set('display_errors', 1); 
+error_reporting(E_ALL);
+s 
 include_once '../config/database.php';
 include_once '../classe/pecas.php';	
 
@@ -15,10 +15,8 @@ $database = new Database();
 $db = $database->getConnection();
 $items = new Computador($db);
 
-// Certifique-se de que o método getPecas() está retornando um PDOStatement
 $stmt = $items->getPecas();
 
-// Verifique se a consulta foi bem-sucedida
 if ($stmt) {
     $itemCount = $stmt->rowCount();
     if ($itemCount > 0) {
@@ -26,7 +24,6 @@ if ($stmt) {
         $computadorArr["body"] = array();
         $computadorArr["itemCount"] = $itemCount;
 
-        // Adicionando cada item à resposta
         while ($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
             extract($row);
             $e = array(
@@ -36,7 +33,6 @@ if ($stmt) {
             array_push($computadorArr["body"], $e);
         }
 
-        // Envia a resposta JSON com os dados
         echo json_encode($computadorArr);
     } else {
         http_response_code(404);
